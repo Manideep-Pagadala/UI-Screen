c1acd11109f16bbd2cbb522ddd6cee5407380d65
manideep <manideep.pagadala@gmail.com>
2024-04-30 08:46:43 +0530

    files added

---
+++ src/app/SideMenu/sidmenu/sidmenu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidmenu',
+  templateUrl: './sidmenu.component.html',
+  styleUrl: './sidmenu.component.css'
+})
+export class SidmenuComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Header/header/header.component';
+import { NavbarComponent } from './NavBar/navbar/navbar.component';
+import { SidmenuComponent } from './SideMenu/sidmenu/sidmenu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    SidmenuComponent
   ],
   imports: [
     BrowserModule,
